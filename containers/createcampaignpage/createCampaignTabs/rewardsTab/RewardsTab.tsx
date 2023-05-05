acec9042d2d55ed0b80231608613280138b03012
xapski <xapski2671@gmail.com>
2023-05-05 03:35:34 +0000

    May 5, 2023, 4:35 AM

--- containers/createcampaignpage/createCampaignTabs/rewardsTab/RewardsTab.tsx
+++ containers/createcampaignpage/createCampaignTabs/rewardsTab/RewardsTab.tsx
@@ -166,6 +166,8 @@ export default function RewardsTab() {
       setRDelD("")
       setRQty("")
       setItemArr([])
+      setImgURLToBe("")
+      setImgState("unset")
       setSelectedOption([])
       setCurrItem("")
       setOnEdit(false)
