c4e49b01afa80ba42bf5dc8d4ecd54e9588a643d
xapski <xapski2671@gmail.com>
2023-05-10 09:22:46 +0000

    May 10, 2023, 10:22 AM

--- components/creatorBioModal/CreatorBioModal.tsx
+++ components/creatorBioModal/CreatorBioModal.tsx
@@ -4,8 +4,9 @@ export default function CreatorBioModal({ data, offMe }:{data:fcmp, offMe:Functi
   return (
     <>
       <div className="tbx-reactive" onClick={()=>{offMe()}}></div>
-      <div className="tbx-modal">
-        <p className="--tbx-still --tbx-bio">{data.bio}</p>
+      <div className="tbx-modal --tbx-bio-modal">
+        <p className="--tbx-still" style={{ "fontWeight":"700" }}>{"About the creator"}</p>
+        <p className="--tbx-bio"><p>{data.bio}</p></p>
         <div className="tbx-cta-cont">
           <button className="tbx-cta" onClick={()=>{offMe()}}>{"Yes"}</button>
           <button className="tbx-cta" onClick={()=>{offMe()}}>{"No"}</button>
