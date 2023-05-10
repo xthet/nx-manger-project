27485663f0dccb80011ae4e71b932b5324bf3ee8
xapski <xapski2671@gmail.com>
2023-05-10 12:02:17 +0000

    May 10, 2023, 1:02 PM

--- containers/homepage/howItWorks/HowItWorks.tsx
+++ containers/homepage/howItWorks/HowItWorks.tsx
@@ -1,4 +1,4 @@
-import { faChartLine, faGift, faHandHoldingDollar } from "@fortawesome/free-solid-svg-icons"
+import { faChartLine, faChartSimple, faGift, faHandHoldingDollar } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Link from "next/link"
 
@@ -12,10 +12,11 @@ export default function HowItWorks() {
 
       <div className="hi-infobox-container fl-bc fl-sa">
         <div className="hi-infobox fl-cl fl-c">
-          <FontAwesomeIcon icon={faChartLine} className="hi-infobox-icon"/>
+          <FontAwesomeIcon icon={faChartSimple} className="hi-infobox-icon"/>
           <div className="hi-infobox-details fl-cl fl-c">
             <h5 className="hi-info-title">{"Start your own campaign"}</h5>
-            <p className="hi-info">{"Start your own crowdfunding campaign by describing your idea, setting a goal and duration, build a following by creating enticing rewards for different levels of donations and secure the funding you need to turn your idea into reality."}</p>
+            <p className="hi-info">{`Start your own crowdfunding campaign by describing your idea, setting a goal and duration, 
+            and creating enticing rewards for donations made.`}</p>
             <Link href="/create-campaign"><button className="hi-info-cta">{"Start your campaign"}</button></Link>
           </div>
         </div>
@@ -24,7 +25,9 @@ export default function HowItWorks() {
           <FontAwesomeIcon icon={faHandHoldingDollar} className="hi-infobox-icon"/>
           <div className="hi-infobox-details fl-cl fl-c">
             <h5 className="hi-info-title">{"Fund a campaign"}</h5>
-            <p className="hi-info">{"Have you found a project idea you care about?, simply click the “fund this project” button to contribute to its success and earn nice rewards in return."}</p>
+            <p className="hi-info">{`Have you found a project idea you care about?, 
+            simply click the “fund this project” button to 
+            contribute to its success and earn nice rewards in return.`}</p>
             <Link href="/campaigns"><button className="hi-info-cta">{"Fund a campaign"}</button></Link>
           </div>
         </div>
@@ -33,7 +36,8 @@ export default function HowItWorks() {
           <FontAwesomeIcon icon={faGift} className="hi-infobox-icon"/>
           <div className="hi-infobox-details fl-cl fl-c">
             <h5 className="hi-info-title">{"Receive rewards"}</h5>
-            <p className="hi-info">{"Earn various rewards for your funding efforts on successful campaigns. Rewards aren't promised but creators will reach out to backers frequently. Manger is not responsible for project claims or reward fulfillment."}</p>
+            <p className="hi-info">{`Earn various rewards for your funding efforts on successful campaigns. 
+            Rewards aren't promised but creators will reach out to backers frequently.`}</p>
             <Link href="/campaigns"><button className="hi-info-cta">{"Fund a campaign"}</button></Link>
           </div>
         </div>
