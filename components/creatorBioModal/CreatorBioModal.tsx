eb4953967e440c4499026fbd7c3a1589a77bd46e
xapski <xapski2671@gmail.com>
2023-05-10 09:59:38 +0000

    May 10, 2023, 10:59 AM

--- components/creatorBioModal/CreatorBioModal.tsx
+++ components/creatorBioModal/CreatorBioModal.tsx
@@ -3,6 +3,7 @@ import { fcmp } from "@/types"
 import { faTwitter } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope, faGlobe } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link"
 
 export default function CreatorBioModal({ data, offMe }:{data:fcmp, offMe:Function}) {
   const { uData } = useQUData(data.creator)
@@ -17,22 +18,30 @@ export default function CreatorBioModal({ data, offMe }:{data:fcmp, offMe:Functi
         </div>}
         <p className="--tbx-bio"><p>{data.bio}</p></p>
         <div className="cbm-contacts">
-          <span className="cbm-contact">
-            <img src="/assets/manger_bio_logo.svg" alt="--" className="cbm-social-icon"/>
-            {uData?.username}
+          <span className="cbm-contact --socials">
+            <Link href={`/profile/${uData!.username}`}>
+              <img src="/assets/manger_bio_logo.svg" alt="--" className="cbm-social-icon"/>
+            </Link>
+            <Link href={data.website}>
+              <FontAwesomeIcon icon={faGlobe} className="cbm-social-icon"/>
+            </Link>
+            <Link href={data.twitter}>
+              <FontAwesomeIcon icon={faTwitter} className="cbm-social-icon"/>
+            </Link>
           </span>
           <span className="cbm-contact">
-            <FontAwesomeIcon icon={faGlobe} className="cbm-social-icon"/>
-            {data.website}
+            <FontAwesomeIcon icon={faEnvelope} className="cbm-email-icon"/>
+            <span>{data.email}</span>
           </span>
-          <span className="cbm-contact">
-            <FontAwesomeIcon icon={faTwitter} className="cbm-social-icon"/>
-            {data.twitter}
-          </span>
-          <span className="cbm-contact">
-            <FontAwesomeIcon icon={faEnvelope} className="cbm-social-icon"/>
-            {data.email}
-          </span>
+          {
+            data.creators.map((creator, index:number)=>{
+              return (
+                <span key={index}>
+                  {creator}
+                </span>
+              )
+            })
+          }
         </div>
       </div>
     </>
