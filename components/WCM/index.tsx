13be64824f1b2d0f83a7d21ac1a9fdd65d345c10
xapski2671 <xapski2671@gmail.com>
2024-01-10 07:01:27 +0100

    rfc_other_wallet

--- components/WCM/index.tsx
+++ components/WCM/index.tsx
@@ -71,13 +71,17 @@ export default function WalletChoiceModal({ offMe }: { offMe: Function }) {
 						className={s.wallet}
 						onClick={() => connect({ connector: connectors[3] })}
 					>
-						<img
-							src="/assets/wallets/rabby.svg"
-							alt="rabby"
-							className={s.wllt_img}
-						/>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							className={s.wllt_ico}
+						>
+							<path d="M20 7V5c0-1.103-.897-2-2-2H5C3.346 3 2 4.346 2 6v12c0 2.201 1.794 3 3 3h15c1.103 0 2-.897 2-2V9c0-1.103-.897-2-2-2zm-2 9h-2v-4h2v4zM5 7a1.001 1.001 0 0 1 0-2h13v2H5z"></path>
+						</svg>
 						<span>
-							{connectors[3].name}
+							{"Other"}
 							{/* {!connectors[3].ready && " (unsupported)"} */}
 							{isLoading &&
 								connectors[3].id === pendingConnector?.id &&
