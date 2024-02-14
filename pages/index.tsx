2c8fba4f9f3b2bb7240570a01a45d67f6594d764
xapski <xapski2671@gmail.com>
2024-02-14 21:21:18 +0100

    open_graph

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,26 @@ export default function Home() {
 				<title>Manger</title>
 				<meta
 					name="description"
-					content="Manger Project - Fundraising on the blockchain"
+					content="Manger - Fundraising on the blockchain"
 				/>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta data-rh="true" property="og:type" content="hero" />
+				<meta data-rh="true" property="og:site_name" content="Manger" />
+				<meta
+					data-rh="true"
+					property="og:image"
+					content="https://manger-project.vercel.app/manger.png"
+				/>
+				<meta
+					data-rh="true"
+					property="og:url"
+					content="https://manger-project.vercel.app/"
+				/>
+				<meta
+					data-rh="true"
+					property="og:title"
+					content="Manger - Fundraising on the blockchain"
+				/>
 				<link rel="icon" href="/assets/manger_favicon.svg" />
 			</Head>
 			<Header />
